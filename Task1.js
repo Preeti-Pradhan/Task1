d4bc5bd25963b4ae3437550ee8f914246eda60a4
Preeti <73239047+Preeti-Pradhan@users.noreply.github.com>
2021-06-01 20:00:05 +0530

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
         // Paste your Config Code
 		apiKey: "AIzaSyCnPgNzXH_hkTkTeSdfQXJFCrj-G23aHYs",
-    authDomain: "tf-firebase-demo-78ccd.firebaseapp.com",
-	databaseURL: "https://tf-firebase-demo-78ccd.firebaseio.com"
-    projectId: "tf-firebase-demo-78ccd",
-    storageBucket: "tf-firebase-demo-78ccd.appspot.com",
-    messagingSenderId: "61525865576",
-    appId: "1:61525865576:web:b61a59a3814f9a6b56b7dd"
+    		authDomain: "tf-firebase-demo-78ccd.firebaseapp.com",
+		databaseURL: "https://tf-firebase-demo-78ccd.firebaseio.com",
+    		projectId: "tf-firebase-demo-78ccd",
+    		storageBucket: "tf-firebase-demo-78ccd.appspot.com",
+    		messagingSenderId: "61525865576",
+    		appId: "1:61525865576:web:b61a59a3814f9a6b56b7dd"
       };
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
